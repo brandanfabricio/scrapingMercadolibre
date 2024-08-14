bd6e90b5c93a59f4013cedf9ea99afd6e4e34334
martincej <nelsoncejas@gmail.com>
2024-08-14 17:18:16 -0300

    test martin

--- scraping/scraping.go
+++ scraping/scraping.go
@@ -34,7 +34,7 @@ func WebScraping(w http.ResponseWriter, r *http.Request) {
 
 }
 */
-// hola
+// hola, como estas ?
 func WebScraping(w http.ResponseWriter, r *http.Request) {
 	var wg sync.WaitGroup
 	resultChan := make(chan map[string]interface{}, 4)
